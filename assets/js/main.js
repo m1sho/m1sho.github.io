bbb4033483cfaebb1efd96e576bd5a1b7224672c
MishoV <popwizardarraki@gmail.com>
2023-10-31 12:00:48 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -292,7 +292,7 @@
 				var $this = $(this);
 
 				// Close.
-					$('<div class="close">X</div>')
+					$('<div class="close">_</div>')
 						.appendTo($this)
 						.on('click', function() {
 							location.hash = '';
